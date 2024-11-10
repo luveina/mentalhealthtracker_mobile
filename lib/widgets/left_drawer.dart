96c144ddcb276b6f4882a500cdc9e65165aab45c
luveina <saragihluvenia@gmail.com>
2024-11-11 01:36:07 +0700

    mengisi TODO

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -28,7 +28,11 @@ class LeftDrawer extends StatelessWidget {
                 Padding(padding: EdgeInsets.all(8)),
                 Text(
                   "Ayo jaga kesehatan mentalmu setiap hari disini!",
-                  // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
@@ -50,10 +54,11 @@ class LeftDrawer extends StatelessWidget {
               title: const Text('Tambah Mood'),
               // Bagian redirection ke MoodEntryFormPage
               onTap: () {
-                /*
-                TODO: Buatlah routing ke MoodEntryFormPage di sini,
-                setelah halaman MoodEntryFormPage sudah dibuat.
-                */
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MoodEntryFormPage(),
+                    ));
               },
             ),
         ],
